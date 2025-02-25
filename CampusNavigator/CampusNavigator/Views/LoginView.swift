3c218c352fa2adecd3884f3880b64237d7ad090d
Siluni <silunienara@gmail.com>
2025-02-25 12:10:00 +0530

    Resolve errors from previous merge conflicts

--- CampusNavigator/CampusNavigator/Views/LoginView.swift
+++ CampusNavigator/CampusNavigator/Views/LoginView.swift
@@ -16,40 +16,6 @@ struct LoginView: View {
     @State private var alertMessage = ""
 
     var body: some View {
-        VStack {
-            Image ("Image")
-            Text("Login")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.largeTitle)
-                .bold()
-
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 15)
-
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.vertical, 15)
-            
-            Button("Continue") {
-                authenticateUser()
-            }
-            .font(.system(size: 18, weight: .semibold))
-            .frame(maxWidth: .infinity, maxHeight: 44)
-            .background(Color.accents)
-            .foregroundColor(.black)
-            .cornerRadius(8)
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
-            
-            HStack {
-                Text("New member?")
-                NavigationLink(destination: RegisterView()) {
-                    Text("Sign Up")
-                        .font(.headline)
-                        .foregroundColor(Color.primarys)
-                        .underline()
         NavigationView{
             VStack {
                 Image ("Image")
@@ -71,7 +37,7 @@ struct LoginView: View {
                 }
                 .font(.system(size: 18, weight: .semibold))
                 .frame(maxWidth: .infinity, maxHeight: 44)
-                .background(Color.accentColor)
+                .background(Color.accents)
                 .foregroundColor(.black)
                 .cornerRadius(8)
                 .alert(isPresented: $showingAlert) {
