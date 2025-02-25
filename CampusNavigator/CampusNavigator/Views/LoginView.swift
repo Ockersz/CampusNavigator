0ba2b3db43556b3c9f6c724f02b6199f92f3e1ce
Siluni <silunienara@gmail.com>
2025-02-25 09:45:34 +0530

    Link Login and Register views

--- CampusNavigator/CampusNavigator/Views/LoginView.swift
+++ CampusNavigator/CampusNavigator/Views/LoginView.swift
@@ -16,48 +16,50 @@ struct LoginView: View {
     @State private var alertMessage = ""
 
     var body: some View {
-        VStack {
-            Image ("Image")
-            Text("Login")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.largeTitle)
-                .bold()
-
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 15)
-
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.vertical, 15)
-            
-            Button("Continue") {
-                authenticateUser()
-            }
-            .font(.system(size: 18, weight: .semibold))
-            .frame(maxWidth: .infinity, maxHeight: 44)
-            .background(Color.accentColor)
-            .foregroundColor(.black)
-            .cornerRadius(8)
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
-            
-            HStack {
-                Text("New member?")
-                NavigationLink(destination: RegisterView()) {
-                    Text("Sign Up")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .underline()
+        NavigationView{
+            VStack {
+                Image ("Image")
+                Text("Login")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.largeTitle)
+                    .bold()
+                
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 15)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.vertical, 15)
+                
+                Button("Continue") {
+                    authenticateUser()
                 }
-                .padding(.vertical, 15)
+                .font(.system(size: 18, weight: .semibold))
+                .frame(maxWidth: .infinity, maxHeight: 44)
+                .background(Color.accentColor)
+                .foregroundColor(.black)
+                .cornerRadius(8)
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+                
+                HStack {
+                    Text("New member?")
+                    NavigationLink(destination: RegisterView()) {
+                        Text("Sign Up")
+                            .font(.headline)
+                            .foregroundColor(.blue)
+                            .underline()
+                    }
+                    .padding(.vertical, 15)
+                }
+                
             }
-        
+            .padding(.horizontal, 32)
         }
-        .padding(.horizontal, 32)
+        .navigationBarBackButtonHidden(true)
     }
-
     private func authenticateUser() {
         let userManager = UserManager()
         
--- CampusNavigator/CampusNavigator/Views/RegisterView.swift
+++ CampusNavigator/CampusNavigator/Views/RegisterView.swift
@@ -16,62 +16,66 @@ struct RegisterView: View {
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     
-    var body: some View {
-        VStack {
-            Image ("Image")
-            Text("Sign Up")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.largeTitle)
-                .bold()
-            
-            TextField("Name", text: $name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 10)
-            
-            TextField("Email", text: $email)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 10)
-
-            SecureField("Password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 10)
-            
-            SecureField("Confirm password", text: $password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.top, 10)
-            
-            Button("Sign up") {
-                registerUser()
-            }
-            .font(.system(size: 18, weight: .semibold))
-            .frame(maxWidth: .infinity, maxHeight: 44)
-            .background(Color.accentColor)
-            .foregroundColor(.black)
-            .cornerRadius(8)
-            .padding(.top, 20)
-            
-            
-        HStack {
-                Text("Already a member?")
-                NavigationLink(destination: RegisterView()) {
-                    Text("Login")
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .underline()
-                }
-            }
-            .padding(.vertical, 15)
-            
-            Text("By Signing up, you are creating a Patron account and agree to Patron’s Terms and Privacy Policy.")
-                .font(.footnote)
-                .multilineTextAlignment(.center)
-        }
-        .padding(.horizontal, 32)
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-        }
-    }
+    @Environment(\.presentationMode) var presentationMode
     
+    var body: some View {
+        NavigationView {
+            VStack {
+                Image ("Image")
+                Text("Sign Up")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.largeTitle)
+                    .bold()
+                
+                TextField("Name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 10)
+                
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 10)
+                
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 10)
+                
+                SecureField("Confirm password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 10)
+                
+                Button("Sign up") {
+                    registerUser()
+                }
+                .font(.system(size: 18, weight: .semibold))
+                .frame(maxWidth: .infinity, maxHeight: 44)
+                .background(Color.accentColor)
+                .foregroundColor(.black)
+                .cornerRadius(8)
+                .padding(.top, 20)
+                
+                
+                HStack {
+                    Text("Already a member?")
+                    Button("Login"){
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .underline()
+                    .font(.headline)
+                    .foregroundColor(.blue)
+                }
+                .padding(.vertical, 15)
+                
+                Text("By Signing up, you are creating a Patron account and agree to Patron’s Terms and Privacy Policy.")
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.horizontal, 32)
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+    }
     func registerUser(){
         let userManager = UserManager()
         
